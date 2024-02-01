14fdbc90ba0c6a46e3d43c5cbaec458802bbe67c
Thomas <thomas.vn@sbidm.com>
2024-02-01 17:32:13 +0700

    them swaggerui

--- Backend/spring-boot-library/src/main/java/com/project/springbootlibrary/SpringBootLibraryApplication.java
+++ Backend/spring-boot-library/src/main/java/com/project/springbootlibrary/SpringBootLibraryApplication.java
@@ -2,8 +2,10 @@ package com.project.springbootlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpringBootLibraryApplication {
 
 	public static void main(String[] args) {
