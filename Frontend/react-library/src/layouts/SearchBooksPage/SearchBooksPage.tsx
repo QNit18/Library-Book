ea8be3e7674b68fca0e12ca902230456d1640127
Thomas <thomas.vn@sbidm.com>
2024-02-13 23:52:05 +0700

    revert fe

--- Frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ Frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -19,7 +19,7 @@ export const SearchBooksPage = () => {
 
     useEffect(() => {
         const fetchBooks = async () => {
-            const baseUrl: string = "https://library-book-production.up.railway.app/api/books";
+            const baseUrl: string = "http://localhost:8080/api/books";
 
             let url: string = '';
 
