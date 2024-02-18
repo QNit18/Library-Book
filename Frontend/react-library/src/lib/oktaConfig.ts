55d8fba0789be3cd1a10d19335d3075411e77e49
Han <hoan08012002hl@gmail.com>
2024-02-18 23:32:33 +0700

    23:32

--- Frontend/react-library/src/lib/oktaConfig.ts
+++ Frontend/react-library/src/lib/oktaConfig.ts
@@ -1,12 +1,6 @@
 export const oktaConfig = {
     clientId: '0oaei2g74oBL00eUa5d7',
-<<<<<<< HEAD
     issuer: 'https://dev-64790387.okta.com/oauth2/default',
-=======
-    // clientId:'0oaeukjq2ceOfGjwt5d7',
-    issuer: 'https://dev-64790387.okta.com/oauth2/default',
-    // issuer:'https://dev-68061772.okta.com/oauth2/default',
->>>>>>> 5e2a23f00285fc6f12ad6fb206a69f6e8606011c
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
